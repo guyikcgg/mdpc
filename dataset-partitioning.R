177658e98b212a8782038a1c0d81d3fe4def8004
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-28 19:23:26 +0200

    Update dataset partitioning

--- dataset-partitioning.R
+++ dataset-partitioning.R
@@ -11,30 +11,30 @@ library(utils)
 library(caret)
 
 # Load the dataset
-data = read.csv("data/datcom2016.csv")
+dataset = read.csv("data/datcom2016.csv")
 
 # Set the random seed in order to be able to reproduce the results
 set.seed(1)
 
 # Create the folds for k-fold cross validation
 folds = createFolds(
-  y = data$class,
+  y = dataset$class,
   k = 10
 )
 
-# Get the folds from data
-split_up <- lapply(folds, function(ind, dat) dat[ind,], dat = data)
+# Get the folds from dataset
+split_up <- lapply(folds, function(ind, dat) dat[ind,], dat = dataset)
 
 # Quick check for the structure
 unlist(lapply(split_up, nrow))
 
 # Function to quickly access to training data for a given fold
-data.train = function(i) {
-  return(do.call("rbind", split_up[-i]))
+dataset.train = function(i) {
+  return(do.call("rbind", splidatat_up[-i]))
 }
 
 # Function to quickly access to test data for a given fold
-data.test = function(i) {
+dataset.test = function(i) {
   return(split_up[[i]])
 }
 
