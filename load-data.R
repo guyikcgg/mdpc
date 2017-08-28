307dbeafc2207f579f24e1da41f551553f0b2fa9
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-28 19:35:40 +0200

    Update load-data.R

--- load-data.R
+++ load-data.R
@@ -12,11 +12,11 @@ library(stats)
 
 
 # Load the complete dataset
-data = read.csv("data/datcom2016.csv")
+dataset = read.csv("data/datcom2016.csv")
 
 
 # Load the 10-folds
-split_up    = list()  # The k-folds (same to data.tst)
+split_up    = list()  # The k-folds (same to dataset.tst)
 for (k in 1:10) {
   filename = paste(
     "data/datcom2016-10-",
@@ -32,40 +32,40 @@ for (k in 1:10) {
   split_up[[k]] = x
 }
 
-# Function to quickly access to training data for a given fold
-data.train = function(i) {
+# Function to quickly access to training set for a given fold
+dataset.train = function(i) {
   return(do.call("rbind", split_up[-i]))
 }
 
-# Function to quickly access to test data for a given fold
-data.test = function(i) {
+# Function to quickly access to test set for a given fold
+dataset.test = function(i) {
   return(split_up[[i]])
 }
 
-# Create lists for useful data
-data.tra    = list()  # Training data
-data.tra.cl = list()  #  |-> Class
-data.tra.dt = list()  #  \-> Data
-data.tst    = list()  # Test data
-data.tst.cl = list()  #  |-> Class
-data.tst.dt = list()  #  \-> Data
+# Create lists for useful sets
+dataset.tra    = list()  # Training set
+dataset.tra.cl = list()  #  |-> Class
+dataset.tra.dt = list()  #  \-> Data
+dataset.tst    = list()  # Test set
+dataset.tst.cl = list()  #  |-> Class
+dataset.tst.dt = list()  #  \-> Data
 for (k in 1:10) {
-  data.tra[[k]]    = data.train(k)
-  data.tra.dt[[k]] = subset((data.train(k)), select = -class)
-  data.tra.cl[[k]] = data.tra[[k]]$class
-  data.tst[[k]]    = data.test(k)
-  data.tst.dt[[k]] = subset((data.test(k)), select = -class)
-  data.tst.cl[[k]] = data.tst[[k]]$class
+  dataset.tra[[k]]    = dataset.train(k)
+  dataset.tra.dt[[k]] = subset((dataset.train(k)), select = -class)
+  dataset.tra.cl[[k]] = dataset.tra[[k]]$class
+  dataset.tst[[k]]    = dataset.test(k)
+  dataset.tst.dt[[k]] = subset((dataset.test(k)), select = -class)
+  dataset.tst.cl[[k]] = dataset.tst[[k]]$class
 }
 
-# Get a sample of the data (one fold is 10%, already many points)
-data.sample = data.test(1)
+# Get a sample of the dataset (one fold is 10%, already many points)
+dataset.sample = dataset.test(1)
 
 # Separate data into numeric and factor (useful for visualization)
-data.numeric = data[,sapply(data, class) != "factor"]
-data.factor  = data[,sapply(data, class) == "factor"]
-data.sample.numeric = data.sample[,sapply(data, class) != "factor"]
-data.sample.factor  = data.sample[,sapply(data, class) == "factor"]
+dataset.numeric = dataset[,sapply(dataset, class) != "factor"]
+dataset.factor  = dataset[,sapply(dataset, class) == "factor"]
+dataset.sample.numeric = dataset.sample[,sapply(dataset, class) != "factor"]
+dataset.sample.factor  = dataset.sample[,sapply(dataset, class) == "factor"]
 
 # Remove dummy variables
 rm(k, filename, x)
