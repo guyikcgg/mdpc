f17f30491ea65370a76ec0bf21c91bb4db15ad2f
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-28 20:16:27 +0200

    Enhance load-data.R

--- load-data.R
+++ load-data.R
@@ -16,7 +16,7 @@ dataset = read.csv("data/datcom2016.csv")
 
 
 # Load the 10-folds
-split_up    = list()  # The k-folds (same to dataset.tst)
+split_up    = list()  # The k-folds (same to dataset.test)
 for (k in 1:10) {
   filename = paste(
     "data/datcom2016-10-",
@@ -42,30 +42,17 @@ dataset.test = function(i) {
   return(split_up[[i]])
 }
 
-# Create lists for useful sets
-dataset.tra    = list()  # Training set
-dataset.tra.cl = list()  #  |-> Class
-dataset.tra.dt = list()  #  \-> Data
-dataset.tst    = list()  # Test set
-dataset.tst.cl = list()  #  |-> Class
-dataset.tst.dt = list()  #  \-> Data
-for (k in 1:10) {
-  dataset.tra[[k]]    = dataset.train(k)
-  dataset.tra.dt[[k]] = subset((dataset.train(k)), select = -class)
-  dataset.tra.cl[[k]] = dataset.tra[[k]]$class
-  dataset.tst[[k]]    = dataset.test(k)
-  dataset.tst.dt[[k]] = subset((dataset.test(k)), select = -class)
-  dataset.tst.cl[[k]] = dataset.tst[[k]]$class
-}
-
-# Get a sample of the dataset (one fold is 10%, already many points)
-dataset.sample = dataset.test(1)
+# Create some useful sets
+dataset.tra    = dataset.train(1)    # Training set
+dataset.tra.cl = dataset.tra$class   #  |-> Class
+dataset.tra.dt = dataset.tra[, -50]  #  \-> Data
+dataset.tst    = dataset.test(1)     # Training set
+dataset.tst.cl = dataset.tst$class   #  |-> Class
+dataset.tst.dt = dataset.tst[, -50]  #  \-> Data
 
 # Separate data into numeric and factor (useful for visualization)
-dataset.numeric = dataset[,sapply(dataset, class) != "factor"]
-dataset.factor  = dataset[,sapply(dataset, class) == "factor"]
-dataset.sample.numeric = dataset.sample[,sapply(dataset, class) != "factor"]
-dataset.sample.factor  = dataset.sample[,sapply(dataset, class) == "factor"]
+dataset.numeric = dataset.tra[,sapply(dataset, class) != "factor"]
+dataset.factor  = dataset.tra[,sapply(dataset, class) == "factor"]
 
 # Remove dummy variables
-rm(k, filename, x)
+rm(filename, x)
