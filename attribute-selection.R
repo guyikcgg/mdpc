39a377bd1d32ba260946f8bad655ad6c1bb594cd
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-30 21:11:51 +0200

    Use the new variable naming convention

--- attribute-selection.R
+++ attribute-selection.R
@@ -13,60 +13,86 @@ library(reshape)
 library(FSelector)
 library(caret)
 
-# Load the data
-source("load-data.R")
+# Visualization
+if (!exists("plot.enable")) {
+  plot.enable = F
+}
+
+# Remove variables
+if (!exists("rm.variables")) {
+  rm.variables = F
+}
 
 # Set the random seed in order to be able to reproduce the results
 set.seed(1)
 
+
 # Get a score for the importance of attributes
 
+numeric.data = subset.numeric.dt(dataset.tra.preprocessed)
+
 ## According to their independence to the class variable
-weights.chi.squared = FSelector::chi.squared(class~., data)
 weights.chi.squared = 
-  weights.chi.squared[order(weights.chi.squared, decreasing = T), , drop = F]
+  chi.squared(class~., dataset.tra.preprocessed)
+
+weights.chi.squared = 
+  weights.chi.squared[
+    order(weights.chi.squared, decreasing = T), , drop = F
+  ]
 
 ## These results can be verified with a KS test, by checking 
 ## for similar distributions between the positive and 
 ## negative class
 ks.test.results.pn = matrix(
-  nrow = ncol(data.numeric), 
+  nrow = ncol(numeric.data), 
   ncol = 1
 )
-rownames(ks.test.results.pn) = names(data.numeric)
-myData.p = subset(cbind(data.numeric, class = data$class), subset = class == "positive")
-myData.n = subset(cbind(data.numeric, class = data$class), subset = class == "negative")
-for (i in 1:(ncol(data.numeric))) {
+rownames(ks.test.results.pn) = names(numeric.data)
+myData.p = subset(
+  subset.numeric(dataset.tra.preprocessed),
+  subset = class == "positive"
+)
+myData.n = subset(
+  subset.numeric(dataset.tra.preprocessed),
+  subset = class == "negative"
+)
+for (i in 1:(ncol(numeric.data))) {
   ks.test.results.pn[i,1] = ks.test(
     myData.p[,i], 
     myData.n[,i]
   )$p.value
 }
 
+## Really non-useful classes
 rownames(ks.test.results.pn)[ks.test.results.pn>0.1]
 
-## In conclusion: having the same distribution for positive and negative is not very informative...
-## We will drop every variable getting 0 score in chi.squared test.
+## Verification of the results of chi.squared test
+rownames(ks.test.results.pn)[ks.test.results.pn>0.00001]
+rownames(weights.chi.squared)[weights.chi.squared == 0]
+
+## In conclusion: having the same distribution for positive and 
+## negative is not very informative... We will drop every 
+## variable getting 0 score in chi.squared test.
 
 # Drop completely irrelevant numeric attributes
 drop.attributes = 
   rownames(weights.chi.squared)[weights.chi.squared == 0]
-drop.attributes = which(names(data.numeric) %in% drop.attributes)
+drop.attributes.n = which(names(numeric.data) %in% drop.attributes)
 
 
 # Look for redundant attributes
 ## Check correlation amongst variables
-correlations = cor(data.numeric)
-drop.attributes = c(
-  drop.attributes,
+correlations = cor(numeric.data)
+drop.attributes.n = c(
+  drop.attributes.n,
   findCorrelation(correlations, cutoff = 0.8)
 )
 
 # Drop the attributes
-drop.attributes = unique(drop.attributes)
-print(paste("Dropping", length(drop.attributes), "attributes..."))
-data.numeric.simplified = 
-  data.numeric[, -drop.attributes]
+drop.attributes.n = unique(drop.attributes.n)
+print(paste("Dropping", length(drop.attributes.n), "attributes..."))
+numeric.data.simplified = 
+  numeric.data[, -drop.attributes.n]
 
 
 
