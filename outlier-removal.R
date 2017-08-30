49df5261b41c8fff017e63fedeb3dd5fbf9eab0a
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-30 13:38:15 +0200

    Use the new convention to get the subsets

--- outlier-removal.R
+++ outlier-removal.R
@@ -16,6 +16,7 @@ if (!exists("plot.enable")) {
   plot.enable = F
 }
 
+# Set the random seed in order to be able to reproduce the results
 set.seed(1)
 
 # A row will be considered an outlier if it presents an
@@ -27,14 +28,14 @@ set.seed(1)
 # Remove univariate outliers (only from numeric data, not integers!)
 
 ## Look for outliers in every column
-my.dataset.dt = dataset.numeric.dt[, sapply(dataset.numeric.dt, class) == "numeric"]
-my.dataset.cl = dataset.numeric.cl
+my.dataset.dt = subset.numeric.dt(dataset.tra)[, sapply(subset.numeric.dt(dataset.tra), class) == "numeric"]
+my.dataset.cl = subset.numeric.cl(dataset.tra)
 my.dataset    = cbind(my.dataset.dt, class = my.dataset.cl)
 removed.rows  = 0
-my.selection  = c()
+UNI.outliers  = c()
 for (i in 1:ncol(my.dataset.dt)) {
     new.data     = rm.outlier(my.dataset.dt[,i], fill = F)
-    my.selection = c(my.selection, which(!(my.dataset.dt[,i] %in% new.data)))
+    UNI.outliers = c(UNI.outliers, which(!(my.dataset.dt[,i] %in% new.data)))
     
     if (plot.enable) {
       # Visualize the removed outliers
@@ -43,20 +44,15 @@ for (i in 1:ncol(my.dataset.dt)) {
           (names(my.dataset)[i]),
           color = "class")) +
           geom_freqpoly(bins = 70) +
-          geom_freqpoly(data = dataset.numeric[my.selection, ], color = "#FFCC00", bins = 70)
+          geom_freqpoly(data = subset.numeric(dataset.tra)[UNI.outliers, ], color = "#FFCC00", bins = 70)
       )
     }
-    
 }
 
 ## Get the rows containing outliers
-my.selection  = unique(my.selection)
-removed.rows  = length(my.selection)
+UNI.outliers  = unique(UNI.outliers)
 
-## Remove the rows containing outliers
-my.dataset    = my.dataset[-my.selection, ]
-my.dataset.cl = dataset.numeric.cl[-my.selection]
-
+removed.rows  = length(UNI.outliers)
 print(paste("Removed", removed.rows, "rows containing univariate outliers"))
 
 
@@ -66,13 +62,13 @@ print(paste("Removed", removed.rows, "rows containing univariate outliers"))
 # Remove multivariate outliers (from both numeric and integer)
 
 ## Select the 10 most useful variables according to chi.squared test
-weights.chi.squared = chi.squared(class~., dataset.numeric)
+weights.chi.squared = chi.squared(class~., subset.numeric(dataset.tra))
 weights.chi.squared =
   weights.chi.squared[order(weights.chi.squared, decreasing = T), , drop = F]
 
-my.selection = names(dataset.numeric.dt) %in% rownames(weights.chi.squared)[1:10]
-my.dataset.10.dt = dataset.numeric.dt[,my.selection]
-my.dataset.10.cl = dataset.numeric.cl
+my.selection = names(subset.numeric.dt(dataset.tra)) %in% rownames(weights.chi.squared)[1:10]
+my.dataset.10.dt = subset.numeric.dt(dataset.tra)[,my.selection]
+my.dataset.10.cl = subset.numeric.cl(dataset.tra)
 my.dataset.10    = cbind(my.dataset.10.dt, class = my.dataset.10.cl)
 
 ## Normalize the data
@@ -86,13 +82,8 @@ is.MCD.outlier = mvoutlier.plot$outliers
 
 ## Get the rows containing outliers
 MCD.outliers = which(is.MCD.outlier)
+
 removed.rows = length(MCD.outliers)
-
-## Remove these outliers from the data
-my.dataset.10.clean.dt = my.dataset.10.dt[-MCD.outliers, ]
-my.dataset.10.clean.cl = my.dataset.10.cl[-MCD.outliers]
-my.dataset.10.clean = cbind(my.dataset.10.clean.dt, class = my.dataset.10.clean.cl)
-
 print(paste("Removed", removed.rows, "rows containing multivariate outliers"))
 
 ## Visualize removed outliers
@@ -106,16 +97,48 @@ if (plot.enable) {
 }
 
 
+# Remove outliers from the data
+the.outliers = unique(c(UNI.outliers, MCD.outliers))
+dataset.tra.preprocessed    = dataset.tra[-the.outliers, ]
+dataset.tra.preprocessed.dt = dataset.tra.dt[-the.outliers, ]
+dataset.tra.preprocessed.cl = dataset.tra.cl[-the.outliers]
+
 ## Check that the removal of outliers is independent to the class the data come from
-new.proportion = table(my.dataset.10.clean.cl)
+new.proportion = table(dataset.tra.preprocessed$class)
 old.proportion = table(dataset$class)
 
-prop.table(new.proportion)
-prop.table(old.proportion)
+print("Class frequency prior outlier removal:")
+print(sprintf("negative: %.2f %%", 100*prop.table(old.proportion)[1]))
+print(sprintf("positive: %.2f %%", 100*prop.table(old.proportion)[2]))
+
+print("Class frequency after outlier removal:")
+print(sprintf("negative: %.2f %%", 100*prop.table(new.proportion)[1]))
+print(sprintf("positive: %.2f %%", 100*prop.table(new.proportion)[2]))
+
 ## Proportions are very similar, i.e. the removal of registers is not conditioned by the class
 ## (it could be thought that the minoritary class would be selected for removals)
 
 
 # Analyze the percentage of removed registers
-1 - nrow(my.dataset.10.clean) / nrow(my.dataset)
-## Around 17% of the registers have been removed
+print(sprintf(
+  "Proportion of removed outliers: %.2f %%", 
+  100*(1 - nrow(dataset.tra.preprocessed) / nrow(my.dataset))
+))
+## Around 18% of the registers have been removed
+
+# Remove temporal variables
+rm(
+  new.proportion,
+  old.proportion,
+  MCD.outliers,
+  UNI.outliers,
+  removed.rows,
+  the.outliers,
+  is.MCD.outlier,
+  alpha.value,
+  myData,
+  my.selection,
+  my.dataset.10,
+  my.dataset.10.cl,
+  my.dataset.10.dt
+)
