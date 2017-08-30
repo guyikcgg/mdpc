d39fe771e3a4efc28d2ccebbd9819fb5df6b26cb
Cristian G Guerrero <guyik.cgg@gmail.com>
2017-08-30 13:39:06 +0200

    Add a script to run every step

---
+++ mdpc-script.R
@@ -0,0 +1,27 @@
+################################################################
+#      Mineria de Datos: Preprocesamiento y Clasificacion      #
+#                                                              #
+# FILE: mdpc-script.R                                          #
+#                                                              #
+# (C) Cristian González Guerrero                               #
+################################################################
+
+# Clear workspace
+closeAllConnections()
+rm(list=ls())
+
+# Visualization
+if (!exists("plot.enable")) {
+  plot.enable = F
+}
+
+# Preprocessing
+source("load-data.R")
+if (plot.enable) {
+  source("initial-visualization.R")
+}
+source("outlier-removal.R")
+source("discretization.R")
+
+
+# Classification
